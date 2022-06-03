538229708cce1ca8905b671d9805fef73eaf7c0c
0RealStart0 <0realstart0@gmail.com>
2022-06-03 20:57:48 +0900

    다크모드 쿠키구현

--- src/components/Main_nav.js
+++ src/components/Main_nav.js
@@ -1,15 +1,16 @@
 import { render } from '../router/router.js';
+import { setCookie, getCookie } from '../utils/cookie.js';
 
 export default class Main_nav {
     constructor($target) {
         this.$target = $target;
         this.$nav;
         this.$child;
-        
+
         this.render();
         this.childComponent;
     }
-    
+
 
     render() {
         console.log('render');
@@ -19,78 +20,83 @@ export default class Main_nav {
         this.$target.appendChild(this.$nav);
         this.$child = document.createElement('div');
         this.$child.className = 'container';
-        
+        this.resetChild();
         this.$target.appendChild(this.$child);
-        
+
         const templet = `<div class="terminal-nav">
-        <div class="terminal-logo">
-        <div class="logo terminal-prompt"><a href="/" class="no-style">Memory Sport</a></div>
+                            <div class="terminal-logo">
+                                <div class="logo terminal-prompt"><a href="/" class="no-style">Memory Sport</a></div>
                             </div>
                             <nav class="terminal-menu">
                             <ul>
                                 <li><a class="menu-item" href="/practice">연습하기</a></li>
                                 <li><a class="menu-item" href="/multiplay">대결하기</a></li>
-                                <li class="dark-mode">⚫️</li>
-                                </ul>
+                                <li class="dark-mode"></li>
+                            </ul>
                             </nav>
-                            </div>`;
-                            this.$nav.innerHTML = templet;
-                            
-                            this.$nav.addEventListener('click', (e) => {
+                        </div>`;
+        this.$nav.innerHTML = templet;
+
+        this.$nav.addEventListener('click', (e) => {
             if (e.target.nodeName !== 'A') return;
             // if (e.target.className !== 'menu-item') return;
             e.preventDefault();
             history.pushState(null, null, e.target.href);
-            console.log(e.target.href,location.pathname);
+            console.log(e.target.href, location.pathname);
             // if (e.target.href.pathname === location.pathname) {
-                //     console.log('리턴');
+            //     console.log('리턴');
             //     return;
             // }
-            if(location.pathname ==='/'){
+            if (location.pathname === '/') {
                 // this.render();
                 this.resetChild();
-            }else{
-                const {component} = render();
+            } else {
+                const { component } = render();
                 this.childComponent = new component(this.$child);
                 // this.componentArr[1] = this.childComponent;
             }
-            
+
         });
         const $dark = this.$nav.querySelector('.dark-mode');
-        
-        if(!this.currentMode){
+
+        this.currentMode = getCookie('isDark');
+        if (!this.currentMode) {
             this.currentMode = window.matchMedia("(prefers-color-scheme: dark)").matches
-            ? "dark"
-            : "light";
-        }else{
+                ? "dark"
+                : "light";
+        } else {
             if (this.currentMode === 'dark') {
                 document.querySelector('body').className = 'dark';
                 this.currentMode = 'dark';
-                $dark.textContent = '⚫️';
+                $dark.textContent = '⚪️';
             } else {
                 document.querySelector('body').className = 'light';
                 this.currentMode = 'light';
-                $dark.textContent = '⚪️';
+                $dark.textContent = '⚫️';
             }
         }
 
-        
+
         $dark.addEventListener('click', (e) => {
             if (this.currentMode === 'dark') {
                 document.querySelector('body').className = 'light';
                 this.currentMode = 'light';
                 e.target.textContent = '⚫️';
+                setCookie('isDark', 'light', { 'max-age': 86400 });
             } else {
                 document.querySelector('body').className = 'dark';
                 this.currentMode = 'dark';
                 e.target.textContent = '⚪️';
+                setCookie('isDark', 'dark', { 'max-age': 86400 });
             }
         });
     }
-    
-    resetChild(){
+
+    resetChild() {
         console.log('reset');
-        this.$child.innerHTML = '';
+        this.$child.innerHTML = ``;
+        
+        
     }
 
 }
